af0cfea92dd20eb8ae9f8363b55e42176d6fa8ad
Rylan Gooch <rylangooch@gmail.com>
2016-09-08 15:43:10 +0100

    User login implemented but not yet functional

--- index.ios.js
+++ index.ios.js
@@ -12,9 +12,9 @@ import {
     Text,
     View,
     TouchableHighlight,
-    AlertIOS,
+    AlertIOS
 } from 'react-native';
-import t from 'tcomb-form-native';
+var t = require('tcomb-form-native');
 
 var STORAGE_KEY = 'id_token';
 
@@ -27,21 +27,84 @@ var Person = t.struct({
 
 const options = {};
 
-
 class ChallengeAccepted extends Component {
+  async _onValueChange(item, selectedValue) {
+    try {
+      await AsyncStorage.setItem(item, selectedValue);
+    } catch (error) {
+      console.log('AsyncStorage error: ' + error.message);
+    }
+  };
+
+  async _getProtectedQuote() {
+    var DEMO_TOKEN =await AsyncStorage.getItem(STORAGE_KEY);
+    fetch("http://localhost:3001/api/protected/random-quote", {
+      method: "GET",
+      headers: {
+        'Authorization': 'Bearer ' + DEMO_TOKEN
+      }
+    })
+    .then((response) => response.text())
+    .then((quote) => {
+      AlertIOS.alert(
+        "Chuck Norris Quote:", quote)
+    })
+    .done();
+  };
+
+  _userSignup() {
+    var value = this.refs.form.getValue();
+    if (value) { // if validation fails, value will be null
+      fetch("http://localhost:3001/users", {
+        method: "POST",
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          username: value.username,
+          password: value.password,
+        })
+      })
+      .then((response) => response.json())
+      .then((responseData) => {
+        this._onValueChange(STORAGE_KEY, responseData.id_token),
+        AlertIOS.alert(
+          "Signup Success!",
+          "Click the button to get a Chuck Norris quote!"
+        )
+      })
+      .done();
+    }
+  };
+
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <View style={styles.row}>
+          <Text style={styles.title}>Signup/Login below for Chuck Norris Quotes!</Text>
+        </View>
+        <View style={styles.row}>
+          <Form
+            ref="form"
+            type={Person}
+            options={options}
+          />
+        </View>
+        <View style={styles.row}>
+          <TouchableHighlight style={styles.button} onPress={this._userSignup} underlayColor='#99d9f4'>
+            <Text style={styles.buttonText}>Signup</Text>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.button} onPress={this._userLogin} underlayColor='#99d9f4'>
+            <Text style={styles.buttonText}>Login</Text>
+          </TouchableHighlight>
+        </View>
+        <View style={styles.row}>
+          <TouchableHighlight onPress={this._getProtectedQuote} style={styles.button}>
+            <Text style={styles.buttonText}>Get a Chuck Norris Quote!</Text>
+          </TouchableHighlight>
+        </View>
       </View>
     );
   }
