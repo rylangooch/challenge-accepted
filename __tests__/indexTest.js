5093c5ed69e22bb3fafe8986ee33cf298423d952
Jason Luong <jasonluong@Jasons-MacBook-Pro.local>
2016-09-12 13:51:12 +0100

    able to show list of challenge titles

--- __tests__/indexTest.js
+++ __tests__/indexTest.js
@@ -1,7 +1,7 @@
 'use strict';
 import 'react-native';
 import React from 'react';
-import ChallengeAccepted from '../index.ios';
+import ChallengeAccepted from "../index.ios";
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
