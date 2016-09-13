9854ab4c51a5143d04548e312d5cb8394420d3f0
Terry <terry@terrytilley.com>
2016-09-13 16:49:27 +0100

    Output comments to scene

--- scenes/challengeView.js
+++ scenes/challengeView.js
@@ -15,6 +15,7 @@ var ChallengeView = React.createClass({
 
   render: function() {
     console.log(this.props.commentJson);
+    console.log(this.props.commentJson.data[0].attributes.message);
 
     return (
       <View style={styles.container}>
@@ -24,6 +25,12 @@ var ChallengeView = React.createClass({
         <View style={styles.messageBox}>
           <Text style={styles.heading}>
             {this.props.challenge.attributes.description}
+          </Text>
+          <Text style={styles.heading}>
+            Comments
+          </Text>
+          <Text style={styles.subtitle}>
+            {this.props.commentJson.data[0].attributes.message}
           </Text>
         </View>
       </View>
