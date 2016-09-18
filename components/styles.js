ffdec45240f5a774bf31585c8ac9b070c80e7e92
Rylan Gooch <rylangooch@gmail.com>
2016-09-18 15:46:24 +0100

    CSS refactored

--- components/styles.js
+++ components/styles.js
@@ -9,69 +9,21 @@ import {
 } from 'react-native';
 
 const styles = StyleSheet.create({
-  navigator: {
-    flex: 1,
-  },
-  logo: {
-    width: 70,
-    height: 70,
-    alignSelf: 'center'
-  },
-  title: {
-    marginTop:4,
-    fontSize:16,
-    fontWeight: 'bold'
-  },
-  leftNavButtonText: {
+  anteText: {
     fontSize: 18,
-    marginLeft:13,
-    marginTop:2
-  },
-  rightNavButtonText: {
-    fontSize: 18,
-    marginRight:13,
-    marginTop:2
-  },
-  nav: {
-    height: 60,
-    backgroundColor: '#efefef'
-  },
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: '#efefef',
-
-  },
-  containerWelcome: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: '#15204C',
-  },
-  messageBox: {
-    flex: 1,
-    justifyContent: 'center',
-    margin: 10,
-  },
-  playerIcons: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  playerNicknameContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-  },
-  challengeList: {
-    flex: 6,
-    justifyContent: 'center',
-  },
-  badge: {
+    fontWeight: '400',
+    marginTop: 0,
     alignSelf: 'center',
-    height: 110,
-    width: 102,
-    marginBottom: 80,
+    color: '#15204C'
+  },
+  anteTitle: {
+    fontSize: 24,
+    fontWeight: '300',
+    marginTop: 100,
+    marginBottom: 5,
+    alignSelf: 'center',
+    color: '#15204C',
+    fontWeight: '600'
   },
   avatar: {
     alignSelf: 'center',
@@ -79,24 +31,31 @@ const styles = StyleSheet.create({
     width: 200,
     borderRadius: 100,
   },
-  ownerIcon: {
-    height: 100,
-    width: 100,
-    borderRadius: 50,
-    marginLeft: 40,
+  badge: {
+    alignSelf: 'center',
+    height: 110,
+    width: 102,
+    marginBottom: 80,
+  },
+  button: {
+    height: 50,
+    alignSelf: 'stretch',
+    backgroundColor: '#FFF',
+    margin: 10,
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontSize: 15,
+    color: '#000000',
+    alignSelf: 'center'
   },
   challengerIcon: {
     height: 100,
     width: 100,
     borderRadius: 50,
     marginRight: 40,
-  },
-  ownerName: {
-    fontSize: 22,
-    fontWeight: '400',
-    alignSelf: 'center',
-    color: '#FFF',
-    marginLeft: 10,
   },
   challengerName: {
     fontSize: 22,
@@ -105,57 +64,19 @@ const styles = StyleSheet.create({
     color: '#FFF',
     marginRight: 10,
   },
-  subtitle: {
-    fontSize: 17,
-    fontStyle: 'italic',
-    textAlign: 'center',
-    marginTop: 4,
-    color: '#15204C',
+  challengeList: {
+    flex: 6,
+    justifyContent: 'center',
   },
-  subtitleWelcome: {
-    fontSize: 17,
-    fontStyle: 'italic',
-    textAlign: 'center',
-    marginTop: 4,
-    color: '#fff',
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: '#efefef',
   },
-  versus: {
-    fontSize: 30,
-    fontWeight: '300',
-    textAlign: 'center',
-    marginTop: 30,
-    marginRight: 5,
-    color: '#15204C',
-  },
-  welcome_title: {
-    fontSize: 18,
-    textAlign: 'center',
-    marginTop: 20,
-    color: '#FFFFFF',
-  },
-  title: {
-    fontSize: 28,
-    fontWeight: '300',
-    textAlign: 'center',
-    marginTop: 20,
-    marginBottom: 5,
-    color: '#15204C',
-  },
-  titleWelcome: {
-    fontSize: 28,
-    fontWeight: '300',
-    textAlign: 'center',
-    marginTop: 20,
-    marginBottom: 5,
-    color: '#fff',
-  },
-  welcomeTitle: {
-    fontSize: 18,
-    fontWeight: '300',
-    textAlign: 'center',
-    marginTop: 20,
-    marginBottom: 5,
-    color: '#15204C',
+  containerWelcome: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: '#15204C',
   },
   createChallengeButton: {
     height: 50,
@@ -166,17 +87,31 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  mainButton: {
-    height: 50,
-    alignSelf: 'stretch',
-    backgroundColor: '#15204C',
-    margin: 10,
-    borderRadius: 5,
-    justifyContent: 'center',
-    alignItems: 'center',
+  description: {
+    fontSize: 18,
+    fontWeight: '400',
+    marginTop: 40,
+    alignSelf: 'center',
+    color: '#15204C'
   },
-  mainButtonText: {
-    color: '#ffffff',
+  dropDown: {
+    width: 355,
+    fontSize: 14,
+    height: 50,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#c6c5c5',
+    justifyContent: 'center',
+    backgroundColor: '#FFF',
+    margin: 10,
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: '600',
+    marginTop: 100,
+    marginBottom: 40,
+    alignSelf: 'center',
+    color: '#15204C'
   },
   image: {
     flex: 3,
@@ -184,30 +119,6 @@ const styles = StyleSheet.create({
     width: 300,
     height: 180,
     marginTop: 100
-  },
-  signInButton: {
-    height: 50,
-    alignSelf: 'stretch',
-    backgroundColor: '#15204C',
-    margin: 10,
-    borderRadius: 5,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  signInButtonText: {
-    color: '#ffffff',
-  },
-  welcomeButton: {
-    height: 50,
-    alignSelf: 'stretch',
-    backgroundColor: '#fff',
-    margin: 10,
-    borderRadius: 5,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  welcomeButtonText: {
-    color: '#15204C',
   },
   input: {
     height: 50,
@@ -231,25 +142,118 @@ const styles = StyleSheet.create({
     borderColor: '#c6c5c5',
     backgroundColor: '#FFF'
   },
-  dropDown: {
-    width: 355,
-    fontSize: 14,
-    height: 50,
-    borderRadius: 5,
-    borderWidth: 1,
-    borderColor: '#c6c5c5',
-    justifyContent: 'center',
-    backgroundColor: '#FFF',
-    margin: 10,
+
+  leftNavButtonText: {
+    fontSize: 18,
+    marginLeft:13,
+    marginTop:2
   },
-  button: {
+  logo: {
+    width: 70,
+    height: 70,
+    alignSelf: 'center'
+  },
+  mainButton: {
     height: 50,
     alignSelf: 'stretch',
-    backgroundColor: '#FFF',
+    backgroundColor: '#15204C',
     margin: 10,
     borderRadius: 5,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  mainButtonText: {
+    color: '#ffffff',
+  },
+  messageBox: {
+    flex: 1,
+    justifyContent: 'center',
+    margin: 10,
+  },
+  nav: {
+    height: 60,
+    backgroundColor: '#efefef'
+  },
+  navigator: {
+    flex: 1,
+  },
+  ownerIcon: {
+    height: 100,
+    width: 100,
+    borderRadius: 50,
+    marginLeft: 40,
+  },
+  ownerName: {
+    fontSize: 22,
+    fontWeight: '400',
+    alignSelf: 'center',
+    color: '#FFF',
+    marginLeft: 10,
+  },
+  playerIcons: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  playerNicknameContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  rightNavButtonText: {
+    fontSize: 18,
+    marginRight:13,
+    marginTop:2
+  },
+  signInButton: {
+    height: 50,
+    alignSelf: 'stretch',
+    backgroundColor: '#15204C',
+    margin: 10,
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  signInButtonText: {
+    color: '#ffffff',
+  },
+  subtitle: {
+    fontSize: 17,
+    fontStyle: 'italic',
+    textAlign: 'center',
+    marginTop: 4,
+    color: '#15204C',
+  },
+  subtitleWelcome: {
+    fontSize: 17,
+    fontStyle: 'italic',
+    textAlign: 'center',
+    marginTop: 4,
+    color: '#fff',
+  },
+
+
+  title: {
+    fontSize: 28,
+    fontWeight: '300',
+    textAlign: 'center',
+    marginTop: 20,
+    marginBottom: 5,
+    color: '#15204C',
+  },
+  title: {
+    marginTop:4,
+    fontSize:16,
+    fontWeight: 'bold'
+  },
+  titleWelcome: {
+    fontSize: 28,
+    fontWeight: '300',
+    textAlign: 'center',
+    marginTop: 20,
+    marginBottom: 5,
+    color: '#fff',
   },
   touchableHighlight: {
     padding: 10,
@@ -276,18 +280,39 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     flexWrap: 'wrap'
   },
-  buttonText: {
-    fontSize: 15,
-    color: '#000000',
-    alignSelf: 'center'
+  versus: {
+    fontSize: 30,
+    fontWeight: '300',
+    textAlign: 'center',
+    marginTop: 30,
+    marginRight: 5,
+    color: '#15204C',
   },
-  heading: {
-    fontSize: 24,
-    fontWeight: '600',
-    marginTop: 100,
-    marginBottom: 40,
-    alignSelf: 'center',
-    color: '#15204C'
+  welcome_title: {
+    fontSize: 18,
+    textAlign: 'center',
+    marginTop: 20,
+    color: '#FFFFFF',
+  },
+  welcomeTitle: {
+    fontSize: 18,
+    fontWeight: '300',
+    textAlign: 'center',
+    marginTop: 20,
+    marginBottom: 5,
+    color: '#15204C',
+  },
+  welcomeButton: {
+    height: 50,
+    alignSelf: 'stretch',
+    backgroundColor: '#fff',
+    margin: 10,
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  welcomeButtonText: {
+    color: '#15204C',
   },
   winnerTitle: {
     fontSize: 24,
@@ -298,33 +323,10 @@ const styles = StyleSheet.create({
     color: '#15204C',
     fontWeight: '600'
   },
-  anteTitle: {
-    fontSize: 24,
-    fontWeight: '300',
-    marginTop: 100,
-    marginBottom: 5,
-    alignSelf: 'center',
-    color: '#15204C',
-    fontWeight: '600'
-  },
-  anteText: {
-    fontSize: 18,
-    fontWeight: '400',
-    marginTop: 0,
-    alignSelf: 'center',
-    color: '#15204C'
-  },
   winnerBox: {
     flex: 1,
     justifyContent: 'center',
     marginBottom: 100,
-  },
-  description: {
-    fontSize: 18,
-    fontWeight: '400',
-    marginTop: 40,
-    alignSelf: 'center',
-    color: '#15204C'
   },
 });
 
